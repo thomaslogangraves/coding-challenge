1ad01e8b11de06b036fe94bce6e61c6cc97c1e2b
Thomas Graves <thomaslogangraves@gmail.com>
2016-12-13 16:10:04 -0500

    add in ambassador to the model

--- referly/api/models.py
+++ referly/api/models.py
@@ -3,3 +3,4 @@ from django.db import models
 class Referral(models.Model):
     title = models.CharField(max_length=30, unique=True)
     clicks = models.IntegerField(default=0)
+    ambassador = models.ForeignKey('auth.User', related_name='referrals', on_delete=models.CASCADE)
