7c9bcc21a2a08deee6706e7e9f5240bf396e1ff2
Thomas Graves <thomaslogangraves@gmail.com>
2016-12-13 17:00:48 -0500

    finish out edits to make permissons on referrals

--- referly/api/serializers.py
+++ referly/api/serializers.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import User
 from api.models import Referral
 
 class ReferralSerializer(serializers.ModelSerializer):
-    ambassador = serializers.ReadOnlyField(source='owner.username')
+    owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = Referral
-        fields = ('id', 'title', 'clicks', 'ambassador')
+        fields = ('id', 'title', 'clicks', 'owner')
 
 class UserSerializer(serializers.ModelSerializer):
     referrals = serializers.PrimaryKeyRelatedField(many=True, queryset=Referral.objects.all())
--- referly/api/views.py
+++ referly/api/views.py
@@ -2,20 +2,24 @@ from api.models import Referral
 from api.serializers import ReferralSerializer, UserSerializer
 from rest_framework import generics, permissions
 from django.contrib.auth.models import User
+from api.permissions import IsOwnerOrReadOnly
 
 class ReferralList(generics.ListCreateAPIView):
     queryset = Referral.objects.all()
     serializer_class = ReferralSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                      IsOwnerOrReadOnly,)
 
     def perform_create(self, serializer):
-    serializer.save(ambassador=self.request.user)
+        serializer.save(owner=self.request.user)
 
 
 class ReferralDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Referral.objects.all()
     serializer_class = ReferralSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                      IsOwnerOrReadOnly,)
+
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
