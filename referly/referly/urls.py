2edb6851c2ef0969a68d43be9b560deb15e39f1e
Thomas Graves <thomaslogangraves@gmail.com>
2016-12-12 19:45:58 -0500

    make small changes to fix imports

--- referly/referly/urls.py
+++ referly/referly/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from rest_framework import routers
-from tutorial.quickstart import views
+from referrals import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
