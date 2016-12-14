7814efca6824bd31b07761bd421d3e02b066e36f
Thomas Graves <thomaslogangraves@gmail.com>
2016-12-14 15:14:21 -0500

    add in support for schema

--- referly/api/urls.py
+++ referly/api/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url, include
 from api import views
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
+
+schema_view = get_schema_view(title='Pastebin API')
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
@@ -9,6 +12,7 @@ router.register(r'users', views.UserViewSet)
 
 
 urlpatterns = [
+    url('^schema/$', schema_view),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
