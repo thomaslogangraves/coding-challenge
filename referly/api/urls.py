c98756a7abf4661b751b9aa848e9d19b3743392a
Thomas Graves <thomaslogangraves@gmail.com>
2016-12-14 15:04:28 -0500

    finish config for urls

--- referly/api/urls.py
+++ referly/api/urls.py
@@ -1,16 +1,14 @@
 from django.conf.urls import url, include
-from rest_framework.urlpatterns import format_suffix_patterns
 from api import views
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = format_suffix_patterns([
-    url(r'^$', views.api_root),
-    url(r'^referrals/$', views.ReferralList.as_view(), name='referral-list'),
-    url(r'^referrals/(?P<pk>[0-9]+)/$', views.ReferralDetail.as_view(), name='referral-detail'),
-    url(r'^users/$', views.UserList.as_view(), name='user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail'),
-])
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'referrals', views.ReferralViewSet)
+router.register(r'users', views.UserViewSet)
 
-urlpatterns += [
-    url(r'^api-auth/', include('rest_framework.urls',
-                               namespace='rest_framework')),
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
