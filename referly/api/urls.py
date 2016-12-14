b15bf22dbe848fd7051c94eeab9aa1ec8071f67e
Thomas Graves <thomaslogangraves@gmail.com>
2016-12-14 15:41:41 -0500

    first setup for heroku- whitenoise profile and databaseurls

--- referly/api/urls.py
+++ referly/api/urls.py
@@ -3,7 +3,7 @@ from api import views
 from rest_framework.routers import DefaultRouter
 from rest_framework.schemas import get_schema_view
 
-schema_view = get_schema_view(title='Pastebin API')
+schema_view = get_schema_view(title='Referrals API')
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
--- referly/referly/settings.py
+++ referly/referly/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -121,8 +121,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 REST_FRAMEWORK = {
     'PAGE_SIZE': 10
 }
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
